7fe4d689aaa301999f139e8d056625cabb832048
noman <syful@loosemonkies.com>
2013-01-13 02:37:49 +0600

    After some file missing all those are restored

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('rest','curl');
+$autoload['libraries'] = array();
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('rest','curl');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','file');
+$autoload['helper'] = array();
 
 
 /*
--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://".$_SERVER['HTTP_HOST'];
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'VFLUYHVBDFUHFDLGjkhdfjvhdlkhbjdlfvghlkd';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
